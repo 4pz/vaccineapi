b360094b6abca392e89160741dc4e3f0ddac78d8
Vinay <vinayven01@gmail.com>
2021-04-06 15:59:42 -0700

    reqData

--- main.go
+++ main.go
@@ -3,33 +3,65 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 type Stats struct {
-	dosesGiven string `json:"dosesGiven"`
-	newDosesGiven string `json:"newDosesGiven"`
-	fullyVaccinated string `json:"fullyVaccinated"`
-	percentFullyVaccinated string `json:"percentFullyVaccinated"`
+	DosesGiven             string `json:"dosesGiven"`
+	NewDosesGiven          string `json:"newDosesGiven"`
+	FullyVaccinated        string `json:"fullyVaccinated"`
+	PercentFullyVaccinated string `json:"percentFullyVaccinated"`
 }
 
 type Articles []Stats
 
-/*func reqData() {
+type Data struct {
+	Country string `json:"country"`
+	IsoCode string `json:"iso_code"`
+	Data    []struct {
+		Date                        string
+		TotalVaccinations           int     `json:"total_vaccinations"`
+		PeopleVaccinated            int     `json:"people_vaccinated"`
+		TotalVaccinationsPerHundred float32 `json:"total_vaccinations_per_hundred"`
+		PeopleVaccinatedPerHundred  float32 `json:"people_vaccinated_per_hundred"`
+		DailyVaccinationsperMillion int     `json:"daily_vaccinations_per_million"`
+	}
+}
 
-}*/
+func reqData() {
+
+	resp, err := http.Get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(body))
+
+	/*var datas []Data
+	json.Unmarshal([]byte(body), &datas)
+	fmt.Printf("Datas : %+v", datas)*/
+
+}
 
 func returnStats(w http.ResponseWriter, r *http.Request) {
 	stats := Articles{
-		Stats{dosesGiven: "1024", newDosesGiven: "1024", fullyVaccinated: "1024", percentFullyVaccinated: "1024"},
+		Stats{DosesGiven: "1024", NewDosesGiven: "1024", FullyVaccinated: "1024", PercentFullyVaccinated: "1024"},
 	}
 
 	fmt.Println("Endpoint Hit: All Stats Endpoint")
 	json.NewEncoder(w).Encode(stats)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
@@ -41,5 +73,6 @@ func handleRequests() {
 }
 
 func main() {
-	handleRequests()
+	//handleRequests()
+	reqData()
 }
